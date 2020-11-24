68e7d98213532bb43d70b9a6601cd13b0a5e3411
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-24 23:07:09 +0100

    test 8 green

--- src/sample/Journal.py
+++ src/sample/Journal.py
@@ -3,9 +3,19 @@ class Journal:
         self.students = {}
 
     def addStudent(self, name, surname, id):
+
         if type(name) != str or type(surname) != str:
             raise TypeError('Must be a string')
+
         if type(id) != int:
             raise TypeError('Id must be a unique number')
+
+        if self.students.get(id, -1) != -1:
+            raise Exception('Id must be unique')
+
         self.students[id] = {'name': name, 'surname': surname}
         return id
+
+
+j = Journal()
+j.addStudent('mac', 'kek', 2)
