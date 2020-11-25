70d7c0048f9a5f8dab41bb5745aeda7a04201289
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-25 22:16:10 +0100

    test 14 15 16 17 parametrized red

--- tests/test_Journal_hamcrest_test.py
+++ tests/test_Journal_hamcrest_test.py
@@ -1,6 +1,7 @@
 from hamcrest import *
 from src.sample.Journal import Journal
 import unittest
+from parameterized import parameterized
 
 class TestJournal(unittest.TestCase):
     def setUp(self):
@@ -12,7 +13,18 @@ class TestJournal(unittest.TestCase):
     def test_edit_student4(self):
         self.tmp.addStudent('Maciej', 'Testowy', 3)
         self.tmp.editStudent(3, 'Inny')
-        assert_that(self.tmp.students['surname'], equal_to('Testowy'))
+        assert_that(self.tmp.students[3]['surname'], equal_to('Testowy'))
+
+    @parameterized.expand([
+        (True, 'Maciej', 'Test'),
+        (3, 4, 'Test'),
+        (3, 'Maciej', False),
+        (4, 'Maciej', 'Test')
+    ])
+    def test_exception_edit_student(self, studid, name, surname):
+        self.tmp.addStudent('Maciej', 'Testowy', 3)
+        assert_that(calling(self.tmp.editStudent).with_args(studid, name, surname),
+                    raises(Exception))
 
     def tearDown(self):
         self.tmp = None
