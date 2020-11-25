7eeb06e9c9da5bacf83494b19b8650f8dc7c07af
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-25 21:28:52 +0100

    test 13 red

--- tests/test_Journal_hamcrest_test.py
+++ tests/test_Journal_hamcrest_test.py
@@ -9,5 +9,10 @@ class TestJournal(unittest.TestCase):
     def test_addStudentHam(self):
         assert_that(self.tmp.addStudent('Aleks', 'Mareks', 3), equal_to(3))
 
+    def test_edit_student4(self):
+        self.tmp.addStudent('Maciej', 'Testowy', 3)
+        self.tmp.editStudent(3, 'Inny')
+        assert_that(self.tmp.students['surname'], equal_to('Testowy'))
+
     def tearDown(self):
         self.tmp = None
