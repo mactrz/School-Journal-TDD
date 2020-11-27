ec1eccb6ec18fb261db433242ab3028c298a9bb7
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-27 21:06:49 +0100

    test59 red

--- tests/test_Journal_test.py
+++ tests/test_Journal_test.py
@@ -94,5 +94,12 @@ class TestJournal(unittest.TestCase):
         self.tmp.addGrade(3, 'Przyroda', 4)
         self.assertEqual(self.tmp.editGrade(3, 'Przyroda', 4, 5), 5)
 
+    def test_editGrade1(self):
+        self.tmp.addStudent('Maciej', 'Testowy', 3)
+        self.tmp.addSubject(3, 'Przyroda')
+        self.tmp.addGrade(3, 'Przyroda', 4)
+        self.tmp.editGrade(3, 'Przyroda', 4, 5)
+        self.assertEqual(self.tmp.students[3]['Subjects']['Przyroda'], [5])
+
     def tearDown(self):
         self.tmp = None
