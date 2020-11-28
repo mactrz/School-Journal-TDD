63e070feee2dd2d82845681413dd667fbda93f5d
Maciej_Trzasalski <mtrzsalski@gmail.com>
2020-11-28 14:32:17 +0100

    test 76 red

--- tests/TestJournalAssertpy.py
+++ tests/TestJournalAssertpy.py
@@ -131,5 +131,16 @@ class TestJournal(unittest.TestCase):
         self.tmp.addSubject(3, 'Przyroda')
         assert_that(self.tmp.averageSubject).raises(Exception).when_called_with(3, 'Przyroda')
 
+    def test_averageStudent1(self):
+        self.tmp.addStudent('Maciej', 'Testowy', 3)
+        self.tmp.addSubject(3, 'Przyroda')
+        self.tmp.addGrade(3, 'Przyroda', 4)
+        self.tmp.addGrade(3, 'Przyroda', 5)
+        self.tmp.addSubject(3, 'WF')
+        self.tmp.addGrade(3, 'WF', 2)
+        self.tmp.addGrade(3, 'WF', 2)
+        self.tmp.addGrade(3, 'WF', 4)
+        assert_that(self.tmp.averageStudent(3)).is_close_to(3.55, 0.01)
+
     def tearDown(self):
         self.tmp = None
